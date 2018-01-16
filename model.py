a4b0874145b5ddf15dd73efdd7fb3b0d87298674
Patrick Dammann <github@thesinforest.eu>
2018-01-16 19:06:54 +0100

    ...

--- model.py
+++ model.py
@@ -85,7 +85,8 @@ class NetworkSmall(nn.Module):
         """
 
     def forward(self, x):
-        return self.lin(self.conv(x))
+        x = self.conv(x)
+        return self.lin(x.view(0), -1)
 
 if __name__ == "__main__":
     net = NetworkSmall(54, 4)
