f9019e383fa053e05eb571704fb522b6a74177fa
Patrick Dammann <github@thesinforest.eu>
2018-01-17 19:05:30 +0100

    added some sanity check

--- debug.py
+++ debug.py
@@ -5,6 +5,10 @@ class Debugger:
     def __init__(self, fp, agent):
         self.screens = np.load(fp)
         self.agent   = agent
+        for i, sc1 in enumerate(self.screens):
+            for sc2 in self.screens[:i]:
+                if (sc1 == sc2).all():
+                    print("There are identical screens in the debug data.")
     
     def eval_screens(self):
         X = self.agent.to_var(self.screens)
